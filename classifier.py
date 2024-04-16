a9e74a725b30c2a93012846258b4f0682a7473d9
Mike Muponda <39035429+mikemuponda@users.noreply.github.com>
2024-04-16 18:48:06 +0200

    dataset + dataloader functionality

--- classifier.py
+++ classifier.py
@@ -8,10 +8,8 @@ import random
 import os
 
 DATA_DIR=""
-print(os.listdir())
 if('MNIST data' in os.listdir()):
     DATA_DIR="./MNIST data"
-    print("TRUE")
 else:
     DATA_DIR = "."
 
@@ -20,4 +18,31 @@ download_dataset = False
 train_mnist = datasets.MNIST(DATA_DIR, train=True, download=download_dataset)
 test_mnist = datasets.MNIST(DATA_DIR, train=False, download=download_dataset)
 
-print(train_mnist)
+X_train = train_mnist.data.float()
+y_train = train_mnist.targets
+X_test = test_mnist.data.float()
+y_test = test_mnist.targets
+# Sample random indices for validation
+test_size = X_test.shape[0]
+indices = np.random.choice(X_train.shape[0], test_size, replace=False)
+
+# Create validation set
+X_valid = X_train[indices]
+y_valid = y_train[indices]
+
+# Remove validation set from training set
+X_train = np.delete(X_train, indices, axis=0)
+y_train = np.delete(y_train, indices, axis=0)
+
+X_train = X_train.reshape(-1, 28*28)
+X_valid = X_valid.reshape(-1, 28*28)
+X_test = X_test.reshape(-1, 28*28)
+
+batch_size = 64
+train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
+val_dataset = torch.utils.data.TensorDataset(X_valid, y_valid)
+test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
